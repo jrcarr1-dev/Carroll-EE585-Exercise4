32d615b3ca61ade472705c30742ffcda0315311a
21carrolljr <21carrolljr@macbookair.mynetworksettings.com>
2026-02-12 18:28:14 -0500

    rMR restored

--- Main.R
+++ Main.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(neon4cast)
 library(lubridate)
 #devtools::install_version("rMR", version = "1.1.0")
-#library(rMR)
+library(rMR)
 
 forecast_date <- Sys.Date()
 noaa_date <- Sys.Date() - days(1)  #Need to use yesterday's NOAA forecast because today's is not available yet
